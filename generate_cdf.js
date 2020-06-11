eefa87e3e838485c90d0343dba86935d16b7e8c3
ryanpeiffer <ryanpeiff@gmail.com>
2020-06-10 19:49:52 -0500

    added more events and table

--- generate_cdf.js
+++ generate_cdf.js
@@ -1,7 +1,9 @@
-
+//Global variable declaration
 const t = 500; //duration for transitions
+var prob_interval = .03; //margin of error for filtering common probability events
+var tbl_pcts = [0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.99, 0.999]; //values to be shown in table output
 
-//I'm pretty sure this is bad practice but I'm so confused at this point how to make it work
+//I'm pretty sure this global variable is bad practice but I haven't figured out how to refactor it better yet
 var event_data = [];
 d3.csv("events.csv", function(data) {
 	data.forEach(function(d) {
@@ -15,12 +17,12 @@ d3.csv("events.csv", function(data) {
 });
 
 
-// set the dimensions and margins of the graph
+//set the dimensions and margins of the graph
 var margin = {top: 10, right: 30, bottom: 30, left: 55},
     width = 480 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    height = 360 - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
+//append the svg object to the body of the page
 var svg = d3.select("#cdf_graph")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -29,14 +31,14 @@ var svg = d3.select("#cdf_graph")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
-// Initialise a X axis:
+//initialize X axis
 var x = d3.scaleLinear().range([0,width]);
 var xAxis = d3.axisBottom().scale(x);
 svg.append("g")
   .attr("transform", "translate(0," + height + ")")
   .attr("class","myXaxis");
 
-//Text label for X axis
+//text label for X axis
 svg.append("text")             
     .attr("transform",
           "translate(" + (width/2) + " ," + 
@@ -44,14 +46,14 @@ svg.append("text")
     .style("text-anchor", "middle")
     .text("Number of Kills");
 
-// Initialize an Y axis
+//initialize Y axis
 var y = d3.scaleLinear().range([height, 0]);
 var yAxis = d3.axisLeft().scale(y)
 	.tickFormat(d3.format(",.0%"));
 svg.append("g")
   .attr("class","myYaxis");
 
-//Text label for Y axis
+//text label for Y axis
 svg.append("text")
   .attr("transform", "rotate(-90)")
   .attr("y", 3 - margin.left)
@@ -60,11 +62,11 @@ svg.append("text")
   .style("text-anchor", "middle")
   .text("Probability of going dry"); 
 
-
-//function that updates the plot
+//function to update graph and numbers on page
+//will run when droprate or killcount is changed
 function update() {
 
-	//update dataset
+	//update dataset for graph
 	droprate = document.getElementById("droprate").value;
 	killcount = document.getElementById("killcount").value;
 	var cdf_data = [];
@@ -74,25 +76,25 @@ function update() {
 	  cdf_data.push({kills: kill, probs: prob});
 	}
 
-	// Create the X axis:
+	//create X axis
 	x.domain([0, d3.max(cdf_data, function(d) { return d.kills }) ]);
 	svg.selectAll(".myXaxis")
 		.transition()
 	  .duration(t)
 	  .call(xAxis);
 
-	// create the Y axis
+	//create Y axis
 	y.domain([0, 1]);
 	svg.selectAll(".myYaxis")
 	  .transition()
 	  .duration(t)
 	  .call(yAxis);
 
-	// update data for the line
+	//update data for line
 	var myline = svg.selectAll(".cdfLine")
 	  .data([cdf_data], function(d){ return d });
 
-	// Update the line
+	//update line
 	myline
 	  .enter()
 	  .append("path")
@@ -105,7 +107,7 @@ function update() {
 	    .y(function(d) { return y(d.probs); }));
 	 myline.exit().remove();
 
-	//Update data for killcount dot
+	//update data for killcount dot
 	var kcData = cdf_data.filter(cdf_data => cdf_data.kills == killcount);
 	if(kcData.length < 1) {
 		kcData = cdf_data.slice(cdf_data.length - 1);
@@ -113,7 +115,7 @@ function update() {
 	var kcDot = svg.selectAll(".kcDot")
 	  .data(kcData);
 
-	//Update the killcount dot
+	//update killcount dot
 	 kcDot
 	  .enter()
 	  .append("circle")
@@ -125,25 +127,41 @@ function update() {
 	  .attr("cx", function(d) {return x(d.kills) })
 	  .attr("cy", function(d) {return y(d.probs) })
 
-	//Make sure dot is drawn in front of the line
+	//make sure dot is drawn in front of the line
 	kcDot.raise();
 
+	//update table
+	var kcTable = document.getElementById('kcTable');
+	for (var i = 0; i < tbl_pcts.length; i++) {
+		tbl_pct = (tbl_pcts[i]*100).toFixed(1)+"%"
+		tbl_kc = Math.floor(Math.log(1-tbl_pcts[i]) / Math.log(1-(1/droprate)));
+		kcTable.rows[0].cells[i].innerHTML=tbl_pct;
+		kcTable.rows[1].cells[i].innerHTML=tbl_kc;
+	}
+
 	//update text below graph
-	var kc_span = document.getElementById('kc_span')
-		.textContent = killcount;
+	document.getElementById('kc_span').textContent = killcount;
 
 	var kcProb = (1 - (1/droprate))**killcount;
-	var kcprob_span = document.getElementById('kcprob_span')
-		.textContent = (kcProb*100).toFixed(2);
+	document.getElementById('kcprob_span').textContent = (kcProb*100).toFixed(2);
 
-	var prob_events = event_data.filter(event_data => event_data.prob < (kcProb + .02) && event_data.prob > (kcProb - .02));
+	//for low-probability events, reduce interval for probability comparisons
+	prob_interval = .03
+	if(kcProb < .01) {
+		prob_interval = .002
+	} else if(kcProb < .05) {
+		prob_interval = .01
+	}
+
+	//select random event that has a similar probability to kcProb
+	var prob_events = event_data.filter(event_data => 
+		event_data.prob < (kcProb + prob_interval) && event_data.prob > (kcProb - prob_interval));
 	if (prob_events.length > 0) {
 		var rand_event = prob_events[Math.floor(Math.random() * prob_events.length)].event;
 	} else {
-		var rand_event = "oops I got nothin";
+		var rand_event = "...oops I don't know! Send me suggestions!";
 	}
-	var prob_event_span = document.getElementById("prob_event_span")
-		.textContent = rand_event
-}
+	document.getElementById("prob_event_span").textContent = rand_event
 
+}
 
